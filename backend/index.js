2aca87d226371cf601f4b58a8932870df10d8d11
rahul24167 <113711057+rahul24167@users.noreply.github.com>
2024-03-05 23:49:18 +0530

    added the index.js for express server and schema for admin table

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require("cors");
+const rootRouter = require("./routes/index");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", rootRouter);
+
+app.listen(3000);
